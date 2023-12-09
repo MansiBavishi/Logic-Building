dc056c5746ff5d074d4ceba21976549756a39dc3
MansiBavishi <mansibavishiinventyv@gmail.com>
2023-12-09 12:05:45 +0530

    Create area.cpp

---
+++ flow-charts/area.cpp
@@ -0,0 +1,17 @@
+// Q1. Area of the circle
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int r, a;
+  cout << "Enter r : ";
+  cin >> r;
+  a = 3.14 * r * r;
+  cout << endl;
+  cout << "Area of the Circle is = " << a;
+  return 0;
+}
+// git branch -M main
+// git push -u origin main
